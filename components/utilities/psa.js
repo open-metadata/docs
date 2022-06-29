77bb6396df75760652ac758ae169375c1b9f2834
Harsh Vador <harsh.vador@somaiya.edu>
2022-06-29 17:07:32 +0530

    (refactor): UI Changes

--- components/utilities/psa.js
+++ components/utilities/psa.js
@@ -1,21 +1,30 @@
 import styles from "./psa.module.css";
+import { ReactComponent as Chat } from "../../images/icons/chat.svg";
 
 const Psa = () => {
   return (
     <section className={styles.Container}>
-      <i className={styles.Icon}>forum</i>
+      <Chat className={styles.Icon} />
       <article>
         <h3 className={styles.Title}>Still have questions?</h3>
+
         <p className={styles.Text}>
-          Our{" "}
+          You can take a look at our{" "}
           <a
-            href="https://discuss.streamlit.io"
+            href="https://github.com/open-metadata/OpenMetadata/discussions/categories/q-a"
             target="_blank"
             className={styles.Link}
           >
-            forums
+            Q&A
           </a>{" "}
-          are full of helpful information and Streamlit experts.
+          or reach out to us in{" "}
+          <a
+            href="https://slack.open-metadata.org/"
+            target="_blank"
+            className={styles.Link}
+          >
+            Slack
+          </a>{" "}
         </p>
       </article>
     </section>
