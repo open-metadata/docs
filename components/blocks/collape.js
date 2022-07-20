5e6a5a3faef7e272555c2cc229395d32ccd7c69b
Harsh Vador <harsh.vador@somaiya.edu>
2022-07-20 19:47:47 +0530

    Usage Collapse UI changes

--- components/blocks/collape.js
+++ components/blocks/collape.js
@@ -4,14 +4,13 @@ import styles from "./collapse.module.css";
 import classNames from "classnames";
 
 const Collapse = ({ title, children }) => {
-  
   const [show, setShow] = useState(false);
 
   return (
     <section className="py-2">
       <div className="collapse_container border border-color_violet rounded">
         <div className="main-heading py-2 px-2" onClick={() => setShow(!show)}>
-          <summary className="flex text-[18px] pl-2 py-2 font-abold">
+          <summary className="flex text-[18px] pl-2 font-abold">
             {title}
             {show ? (
               <SvgArrow className="rotate-90 duration-100 text-xxl fill-color_violet self-center" />
@@ -21,11 +20,15 @@ const Collapse = ({ title, children }) => {
           </summary>
         </div>
         {show && (
-          <div
-            className="main-heading pl-6 pb-2 flex"
-          >
-            <section className={classNames(styles.Container)}>{children}</section>
-            
+          <div className="main-heading pl-6 pb-2 flex">
+            <section
+              className={classNames(
+                styles.Container,
+                styles.collapse_Container
+              )}
+            >
+              {children}
+            </section>
           </div>
         )}
       </div>
